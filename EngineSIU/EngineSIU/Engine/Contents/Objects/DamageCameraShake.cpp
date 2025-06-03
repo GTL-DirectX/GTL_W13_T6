c1293671672b237d788ff9ff60f623cd39a9121c
OhJeongHun <45549589+ojh6507@users.noreply.github.com>
2025-06-04 06:49:23 +0900

    feat: 카메라 흔들림 추가

--- EngineSIU/EngineSIU/Engine/Contents/Objects/DamageCameraShake.cpp
+++ EngineSIU/EngineSIU/Engine/Contents/Objects/DamageCameraShake.cpp
@@ -6,37 +6,38 @@
 
 UDamageCameraShake::UDamageCameraShake()
 {
-    //UWaveOscillatorCameraShakePattern* Pattern = new UWaveOscillatorCameraShakePattern();
     UPerlinNoiseCameraShakePattern* Pattern = new UPerlinNoiseCameraShakePattern();
-    Pattern->Duration = 0.5f;
+
+    // 전체 지속 시간 및 블렌드 설정
+    Pattern->Duration = 0.1f;
     Pattern->BlendInTime = 0.f;
     Pattern->BlendOutTime = 0.f;
 
-    // Rotation
-    Pattern->RotationAmplitudeMultiplier = 1.f;
+    // Rotation (회전) 축 강도 조정
+    Pattern->RotationAmplitudeMultiplier = 0.2f;   // 전체 회전 강도 비율 감소
     Pattern->RotationFrequencyMultiplier = 1.f;
-    
-    Pattern->Yaw.Amplitude = 3.f;
+
+    Pattern->Yaw.Amplitude = .6f;   // 기존 3.0f → 1.5f
     Pattern->Yaw.Frequency = 5.f;
 
-    Pattern->Pitch.Amplitude = 5.f;
-    Pattern->Pitch.Frequency = 4.f;
+    Pattern->Pitch.Amplitude = 2.5f; // 기존 5.0f → 2.5f
+    Pattern->Pitch.Frequency = 2.f;
 
-    Pattern->Roll.Amplitude = 3.f;
-    Pattern->Roll.Frequency = 10.f;
+    Pattern->Roll.Amplitude = 1.5f;  // 기존 3.0f → 1.5f
+    Pattern->Roll.Frequency = 5.f;
 
-    // Location
-    Pattern->LocationAmplitudeMultiplier = 0.f;
+    // Location (위치) 축 강도 조정
+    Pattern->LocationAmplitudeMultiplier = 0.2f;   // 전체 위치 강도 비율 추가/조정
     Pattern->LocationFrequencyMultiplier = 1.f;
 
     Pattern->X.Amplitude = 0.f;
     Pattern->X.Frequency = 1.f;
 
-    Pattern->Y.Amplitude = 5.f;
+    Pattern->Y.Amplitude = 2.f;     // 기존 5.0f → 2.0f
     Pattern->Y.Frequency = 2.f;
 
     Pattern->Z.Amplitude = 0.f;
     Pattern->Z.Frequency = 5.f;
-    
+
     SetRootShakePattern(Pattern);
 }
