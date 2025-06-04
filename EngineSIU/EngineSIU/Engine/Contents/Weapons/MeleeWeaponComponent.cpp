457768ebf451a9b0fdad8e43f0f690f6449e0081
Joochan <joochan123123@gmail.com>
2025-06-04 09:02:37 +0900

    feat: 캐릭터끼리 공격시 추가 딜

--- EngineSIU/EngineSIU/Engine/Contents/Weapons/MeleeWeaponComponent.cpp
+++ EngineSIU/EngineSIU/Engine/Contents/Weapons/MeleeWeaponComponent.cpp
@@ -66,6 +66,7 @@ void UMeleeWeaponComponent::ComponentBeginOverlap(UPrimitiveComponent* Overlappe
                 return;
             }
             FVector DamageDir = OtherActor->GetActorLocation() - GetComponentLocation();
+            OtherPlayer->KnockBackExp *= 2.0f;
             OtherPlayer->OnDamaged(DamageDir);
         }
         if (OtherActor && HitParticle)
